6625a23f416a09c4c930261d9782d1c3c72921a3
Dennis Christensen <drdenner@gmail.com>
2015-10-25 10:00:41 +0100

    Init

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+	public float speed;
+	private Vector3 position;
+	public CharacterController controller;
+	public AnimationClip run;
+	public AnimationClip idle;
+
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Click Mouse Left
+		if (Input.GetMouseButton (0)) {
+			getLocation();
+		}
+		moveToPosition ();	
+	}
+
+	// Get Current Position
+	void getLocation() {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if(Physics.Raycast(ray, out hit, 1000)) {
+			position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+		}
+	
+	}
+
+	void moveToPosition() {
+		if (Vector3.Distance (transform.position, position) > 1.1) {
+			Quaternion newRotation = Quaternion.LookRotation (position - transform.position);
+			newRotation.x = 0f;
+			newRotation.z = 0f;
+			transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * 1000);
+			controller.SimpleMove (transform.forward * speed);
+			GetComponent<Animation>().CrossFade(run.name);
+		} else {
+			GetComponent<Animation>().CrossFade(idle.name);
+
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob : MonoBehaviour {
+	public float speed;
+	public float range;
+	public Transform player;
+	public CharacterController controller;
+	public AnimationClip run;
+	public AnimationClip idle;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!inRange ()) {
+			chase ();
+		} else {
+			GetComponent<Animation>().CrossFade(idle.name);
+		}
+
+	}
+
+	bool inRange() {
+		if (Vector3.Distance (transform.position, player.position) < range) {
+			return true;		
+		} else {
+			return false;
+		}
+	}
+
+	void chase() {
+		transform.LookAt (player.position);
+		controller.SimpleMove (transform.forward * speed);
+		GetComponent<Animation>().CrossFade(run.name);
+	}
+
+	void OnMouseOver() {
+		player.GetComponent<Combat>().opponent = gameObject;
+	}
+}
